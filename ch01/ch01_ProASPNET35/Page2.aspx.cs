409e4fa9efbd0564d44c37f1ccf45c1f22378564
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 12:02:33 -0500

    Listing 1-13. Using the IsCrossPagePostBack Property

--- ch01/ch01_ProASPNET35/Page2.aspx.cs
+++ ch01/ch01_ProASPNET35/Page2.aspx.cs
@@ -7,8 +7,15 @@ namespace ch01_ProASPNET35
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Label1.Text = "Hello " + PreviousPage.pp_TextBox1.Text + "<br />" +
-                "Date Selected: " + PreviousPage.pp_Calendar1.SelectedDate.ToShortDateString();
+            if (PreviousPage != null && PreviousPage.IsCrossPagePostBack)
+            {
+                Label1.Text = "Hello " + PreviousPage.pp_TextBox1.Text + "<br />" +
+                    "Date Selected: " + PreviousPage.pp_Calendar1.SelectedDate.ToShortDateString();
+            }
+            else
+            {
+                Response.Redirect("Page1.aspx");
+            }
         }
     }
 }
