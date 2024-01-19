89810a5d60c174968f6d9c14d271927764a649de
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 11:31:46 -0500

    Listing 1-10. Cross-Page Posting: Page2.aspx

---
+++ ch01/ch01_ProASPNET35/Page2.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Page2.aspx.cs" Inherits="ch01_ProASPNET35.Page2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ch01/ch01_ProASPNET35/Page2.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace ch01_ProASPNET35
+{
+    public partial class Page2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox pp_Textbox1;
+            System.Web.UI.WebControls.Calendar pp_Calendar1;
+
+            pp_Textbox1 = (TextBox)PreviousPage.FindControl("TextBox1");
+            pp_Calendar1 = (Calendar)PreviousPage.FindControl("Calendar1");
+
+            Label1.Text = "Hello " + pp_Textbox1.Text + "<br />" +
+                "Date Selected: " + pp_Calendar1.SelectedDate.ToShortDateString();
+        }
+    }
+}
