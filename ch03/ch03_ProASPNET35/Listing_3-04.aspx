3ed85fa086e0bfbddad6c175f1176191db30e162
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 15:10:26 -0500

    Listing 3-4: Triggering an event when a TextBox change occurs

---
+++ ch03/ch03_ProASPNET35/Listing_3-04.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-04.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_04" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>OnTextChanged Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-04.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_04 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Response.Write("OnTextChanged event triggered");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("OnClick event triggered");
+        }
+    }
+}
