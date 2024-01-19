eff8aa990093a4283a96097a888ed05aa8c117d0
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 15:55:33 -0500

    Listing 3-10: Dynamically generating a DropDownList control from an array

---
+++ ch03/ch03_ProASPNET35/Listing_3-10.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-10.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_10" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Select transportation type:<br />
+            <asp:DropDownList ID="DropDownList1" runat="server" 
+                    OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" 
+                    AutoPostBack="true">
+                <asp:ListItem>Select an Item</asp:ListItem>
+                <asp:ListItem>Car</asp:ListItem>
+                <asp:ListItem>Airplane</asp:ListItem>
+                <asp:ListItem>Train</asp:ListItem>
+            </asp:DropDownList>
+            <asp:DropDownList ID="DropDownList2" runat="server" Visible="false">
+            </asp:DropDownList>
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-10.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] CarArray = new string[4] { "Ford", "Honda", "BMW", "Dodge" };
+            string[] AirplaneArray = new string[3] { "Boeing 777", "Boeing 747", "Boeing 737" };
+            string[] TrainArray = new string[3] { "Bullet Train", "Amtrack", "Tram" };
+
+            if (DropDownList1.SelectedValue == "Car")
+                DropDownList2.DataSource = CarArray;
+            else if (DropDownList1.SelectedValue == "Airplane")
+                DropDownList2.DataSource = AirplaneArray;
+            else
+            {
+                DropDownList2.DataSource = TrainArray;
+            }
+
+            DropDownList2.DataBind();
+            DropDownList2.Visible = true;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Write("You selected <b>" +
+                DropDownList1.SelectedValue.ToString() + ": " +
+                DropDownList2.SelectedValue.ToString() + "</b>");
+        }
+    }
+}
