1bde2ff890667f2d2e10faa747f80d9583460af3
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 15:38:43 -0500

    Listing 3-9: The ImageButton Server Control
    The Click event for the ImageButton control

---
+++ ch03/ch03_ProASPNET35/Listing_3-09.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-09.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_09" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="~/MyButton.JPG" OnClick="ImageButton1_Click"/>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-09.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_09 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Write("Image Button Clicked");
+        }
+    }
+}
