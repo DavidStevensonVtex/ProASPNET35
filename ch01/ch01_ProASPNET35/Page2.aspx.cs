77bb5b8eb7c5490caa215c688ce30769ab801bda
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 11:59:36 -0500

    Listing 1-12. Cross Page Posting: Remove unnecessary code

--- ch01/ch01_ProASPNET35/Page2.aspx.cs
+++ ch01/ch01_ProASPNET35/Page2.aspx.cs
@@ -7,12 +7,6 @@ namespace ch01_ProASPNET35
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            TextBox pp_Textbox1;
-            System.Web.UI.WebControls.Calendar pp_Calendar1;
-
-            pp_Textbox1 = (TextBox)PreviousPage.FindControl("TextBox1");
-            pp_Calendar1 = (Calendar)PreviousPage.FindControl("Calendar1");
-
             Label1.Text = "Hello " + PreviousPage.pp_TextBox1.Text + "<br />" +
                 "Date Selected: " + PreviousPage.pp_Calendar1.SelectedDate.ToShortDateString();
         }
