18d1cffb9707e44353c0fab22c4bc703b0c9b218
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 15:24:09 -0500

    Listing 3-6, 3-7: Constructing multiple Button controls to work from a single function
    Listing 3-7: The Button_Command event

---
+++ ch03/ch03_ProASPNET35/Listing_3-07.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-07.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_07" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Button ID="Button1" runat="server" Text="Button 1" CommandName="DoSomething1" OnCommand="Button_Command" />
+        <asp:Button ID="Button2" runat="server" Text="Button 2" CommandName="DoSomething2" OnCommand="Button_Command" />
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-07.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_07 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Command(object sender, CommandEventArgs e)
+        {
+            switch (e.CommandName)
+            {
+                case "DoSomething1":
+                    Response.Write("Button 1 was selected");
+                    break;
+                case "DoSomething2":
+                    Response.Write("Button 2 was selected");
+                    break;
+            }
+        }
+    }
+}
