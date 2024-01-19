9071dbe86e7be1751d15d30dddc195c8900a1a17
David Stevenson <drstevenson1958@yahoo.com>
2024-01-19 12:19:17 -0500

    Listing 1-14: The Calculator Class

---
+++ ch01/ch01_ProASPNET35/App_Code/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ch01_ProASPNET35.App_Code
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
