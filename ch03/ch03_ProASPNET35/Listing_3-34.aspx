c05c5c320e83d8e117a435a022b39e7a021be509
David Stevenson <drstevenson1958@yahoo.com>
2024-01-20 09:43:11 -0500

    Listing 3-34. Uploading files using the new FileUpload control

---
+++ ch03/ch03_ProASPNET35/Listing_3-34.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-34.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_34" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click"/>
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-34.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_34 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                try
+                {
+                    FileUpload1.SaveAs("D:\\Uploads\\" + FileUpload1.FileName);
+                    Label1.Text = "File name: " +
+                        FileUpload1.PostedFile.FileName + "<br>" +
+                        FileUpload1.PostedFile.ContentLength + " bytes<br>" +
+                        "Content type: " +
+                        FileUpload1.PostedFile.ContentType;
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = "ERROR: " + ex.Message.ToString();
+                }
+            }
+            else
+            {
+                Label1.Text = "You have not specified a file.";
+            }
+        }
+    }
+}
