86832be9beca1b7d45ccf6261b1116c256919b4b
David Stevenson <drstevenson1958@yahoo.com>
2024-01-20 09:12:42 -0500

    Listing 3-28. Using PlaceHOlder to add controls to a page dynamically

---
+++ ch03/ch03_ProASPNET35/Listing_3-28.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-28.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_28" 
+    EnableViewState="false" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-28.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_28 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label newLabelControl = new Label();
+            newLabelControl.Text = "Hello there";
+            PlaceHolder1.Controls.Add(newLabelControl);
+        }
+    }
+}
