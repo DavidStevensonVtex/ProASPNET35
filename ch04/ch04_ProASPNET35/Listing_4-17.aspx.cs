bdeb165991d6f1c0230170118c42b030ee554b05
David Stevenson <drstevenson1958@yahoo.com>
2024-01-20 16:45:00 -0500

    Listing 4-21. Removing the client-side capabilities programmatically

--- ch04/ch04_ProASPNET35/Listing_4-17.aspx.cs
+++ ch04/ch04_ProASPNET35/Listing_4-17.aspx.cs
@@ -12,6 +12,8 @@ namespace ch04_ProASPNET35
         protected void Page_Load(object sender, EventArgs e)
         {
             Label1.Text = string.Empty;
+            RequiredFieldValidator1.EnableClientScript = false;
+            RequiredFieldValidator2.EnableClientScript = false;
         }
 
         protected void Button1_Click(object sender, EventArgs e)
