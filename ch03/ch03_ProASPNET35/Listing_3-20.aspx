c14f1c1c4bf24394987552a9df3019b2ace0bdd5
David Stevenson <drstevenson1958@yahoo.com>
2024-01-20 08:15:12 -0500

    Listing 3-20. Selecting a single day in the Calendar control

---
+++ ch03/ch03_ProASPNET35/Listing_3-20.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-20.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_20" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Using the Calendar Control</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-20.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_20 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Response.Write("You selected: " + Calendar1.SelectedDate.ToShortDateString());
+        }
+    }
+}
