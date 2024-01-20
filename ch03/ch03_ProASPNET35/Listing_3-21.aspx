3cb553b8c2daf308b66516eff13e0281bbb0dcd3
David Stevenson <drstevenson1958@yahoo.com>
2024-01-20 08:23:43 -0500

    Listing 3-21: Retrieving a range of dates from a selection

---
+++ ch03/ch03_ProASPNET35/Listing_3-21.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Listing_3-21.aspx.cs" Inherits="ch03_ProASPNET35.Listing_3_21" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Using the Calendar Control</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Calendar ID="Calendar1" runat="server"
+                OnSelectionChanged="Calendar1_SelectionChanged"
+                SelectionMOde="DayWeekMonth">
+            </asp:Calendar>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ch03/ch03_ProASPNET35/Listing_3-21.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch03_ProASPNET35
+{
+    public partial class Listing_3_21 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            Label1.Text = "<b><u>You selected the following date/dates:</u></b><br>";
+
+            foreach (var date in Calendar1.SelectedDates)
+            {
+                Label1.Text += ((DateTime)date).ToShortDateString() + "<br />";
+            }
+        }
+    }
+}
