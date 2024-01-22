7e27ccfd210c031e3a297694e17c22d6212ac543
David Stevenson <drstevenson1958@yahoo.com>
2024-01-22 16:54:21 -0500

    Listing 5-16. Using OnPreInit to assign the master page programmatically

---
+++ ch05/ch05_ProASPNET35/Listing_5-16.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+
+namespace ch05_ProASPNET35
+{
+    public partial class Listing_5_16 : System.Web.UI.Page
+    {
+        protected override void OnPreInit(EventArgs e)
+        {
+            base.OnPreInit(e);
+            Page.MasterPageFile = "~/MyMasterPage.master";
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
