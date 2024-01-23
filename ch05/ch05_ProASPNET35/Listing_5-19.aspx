4e30db8a2ca4d23fb5a4fd215cdfc8a1b4b05f0a
David Stevenson <drstevenson1958@yahoo.com>
2024-01-23 10:02:15 -0500

    Listing 5-19: The content page

---
+++ ch05/ch05_ProASPNET35/Listing_5-19.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" 
+    MasterPageFile="~/ReutersEurope.Master" 
+    AutoEventWireup="true" 
+    CodeBehind="Listing_5-19.aspx.cs" 
+    Inherits="ch05_ProASPNET35.Listing_5_19" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    Hello World
+</asp:Content>
--- ch05/ch05_ProASPNET35/ReutersEurope.Master.designer.cs
+++ ch05/ch05_ProASPNET35/ReutersEurope.Master.designer.cs
@@ -15,15 +15,6 @@ namespace ch05_ProASPNET35
     {
 
         /// <summary>
-        /// head control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-
-        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -33,12 +24,12 @@ namespace ch05_ProASPNET35
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
-        /// ContentPlaceHolder2 control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder2;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
