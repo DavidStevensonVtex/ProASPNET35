dd83aaebbce8e8e4cc55afc002525726c91ce039
David Stevenson <drstevenson1958@yahoo.com>
2024-01-23 12:54:11 -0500

    Listing 6-13. Assigning the Server control's SkinID property programmatically

---
+++ ch06/ch06_ProASPNET35/Listing_6-13.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" 
+    AutoEventWireup="true" 
+    Theme="Summer"
+    CodeBehind="Listing_6-13.aspx.cs" 
+    Inherits="ch06_ProASPNET35.Listing_6_13" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server">Textbox1</asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ ch06/ch06_ProASPNET35/Listing_6-13.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ch06_ProASPNET35
+{
+    public partial class Listing_6_13 : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            TextBox1.SkinID = "TextboxDashed";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
