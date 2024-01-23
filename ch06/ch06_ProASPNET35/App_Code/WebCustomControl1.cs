6a2add6dec057ffa75c540ea4f92c38398d3c8c3
David Stevenson <drstevenson1958@yahoo.com>
2024-01-23 13:01:03 -0500

    Listing 6-14: Disabling theming for your custom controls

---
+++ ch06/ch06_ProASPNET35/App_Code/WebCustomControl1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlForThemes
+{
+    [DefaultProperty("HeaderText")]
+    [ToolboxData("<{0}:WebCustomControl1 runat='server'></{0}:WebCustomControl1>")];
+    [Themeable(false)]
+    public class WebCustomControl1 : WebControl
+    {
+
+        [Bindable(true)]
+        [Category("Appearance")]
+        [DefaultValue("Enter Value")]
+        [Localizable(true)]
+        public string HeaderText
+        {
+            get
+            {
+                string s = (string)ViewState["HeaderText"];
+                return ((s == null) ? string.Empty : s);)
+            }
+            set
+            {
+                ViewState["HeaderText"] = value;
+            }
+        }
+
+        protected override void RenderControls(HtmlTextWriter output)
+        {
+            output.Write("<h1>" + HeaderText + "<h1>");
+        }
+    }
+}
