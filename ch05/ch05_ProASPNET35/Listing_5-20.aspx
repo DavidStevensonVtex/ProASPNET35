b03caea1be87cffb555a823cfe4101ac7cde050e
David Stevenson <drstevenson1958@yahoo.com>
2024-01-23 10:12:13 -0500

    Listing 5-20. A content page that can work with more than one master page

---
+++ ch05/ch05_ProASPNET35/Listing_5-20.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" 
+    MasterPageFile="~/Wrox.Master" 
+    Mozilla:MasterPageFile="~/WroxMozilla.Master"
+    Opera:MasterPageFile="~/WroxOpera.Master"
+    AutoEventWireup="true" 
+    CodeBehind="Listing_5-20.aspx.cs" 
+    Inherits="ch05_ProASPNET35.Listing_5_20" %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    Header
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    Hello world 1
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    Hello world 2
+</asp:Content>
---
+++ ch05/ch05_ProASPNET35/WroxOpera.Master.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+using Label = System.Web.UI.WebControls.Label;
+
+namespace ch05_ProASPNET35
+{
+    public partial class WroxOpera : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Label1.Text = System.Guid.NewGuid().ToString();
+            }
+        }
+
+        string m_PageHeadingTitle = "My Company";
+
+        public string PageHeadingTitle
+        {
+            get { return m_PageHeadingTitle; }
+            set { m_PageHeadingTitle = value; }
+        }
+
+        public Label MasterPageLabel1
+        {
+            get { return Label1; }
+            set { Label1 = value; }
+        }
+    }
+}
